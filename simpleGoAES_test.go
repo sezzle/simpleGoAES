d4c67ed0555bfeacacccb8e98e8f2bb12bda7b4c
Charlie Youakim <charlie.youakim@sezzle.com>
2016-12-07 10:25:45 -0600

    added more complete testing for many key/text options

--- simpleGoAES_test.go
+++ simpleGoAES_test.go
@@ -1,10 +1,27 @@
 package simpleGoAES
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var runes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.!@#$%^&*(){},=-;")
+var keyLength = 32
+
+func RandString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = runes[rand.Intn(len(runes))]
+	}
+	return string(b)
+}
 
 func TestSimpleGoAES(t *testing.T) {
 	t.Log("Starting encryption.")
@@ -31,66 +48,54 @@ func TestSimpleGoAES(t *testing.T) {
 	//A longer string
 	t.Logf("Testing a longer string.")
 	stringToEncrypt := "Let's encrypt something relatively substantial."
-	t.Logf("String to encrypt = '%s'.", stringToEncrypt)
-	encryptedString, err := EncryptStringToBase64EncodedString(key, stringToEncrypt)
-	if err != nil {
-		t.Fatalf("Could not encrypt the string: %s", err.Error())
-	}
-	decryptedString, err := DecryptBase64StringToString(key, encryptedString)
-	if err != nil {
-		t.Fatalf("Could not decrypt the string: %s", err.Error())
-	}
-	t.Logf("Decrypted string = '%s'.", decryptedString)
+	EncTest(t, key, stringToEncrypt)
 
 	//A super short string
 	t.Logf("Testing a 1-character string.")
 	stringToEncrypt = "A"
-	t.Logf("String to encrypt = '%s'.", stringToEncrypt)
-	encryptedString, err = EncryptStringToBase64EncodedString(key, stringToEncrypt)
-	if err != nil {
-		t.Fatalf("Could not encrypt the string: %s", err.Error())
-	}
-	decryptedString, err = DecryptBase64StringToString(key, encryptedString)
-	if err != nil {
-		t.Fatalf("Could not decrypt the string: %s", err.Error())
-	}
-	t.Logf("Decrypted string = '%s'.", decryptedString)
+	EncTest(t, key, stringToEncrypt)
 
-	assert.Equal(t, stringToEncrypt, decryptedString, "The origin and processed strings were not equal.")
+	//Test something like EIN
+	t.Logf("Testing an EIN string.")
+	stringToEncrypt = "444221111"
+	EncTest(t, key, stringToEncrypt)
 
 	//Test the encryption flag, first off
 	t.Logf("Testing turning off encryption.")
 	IsEncryptionOn = false
 	stringToEncrypt = "Encryption turned off test."
-	t.Logf("String to encrypt = '%s'.", stringToEncrypt)
-	encryptedString, err = EncryptStringToBase64EncodedString(key, stringToEncrypt)
-	if err != nil {
-		t.Fatalf("Could not encrypt the string: %s", err.Error())
-	}
-
-	t.Logf("String as encrypted = '%s'.", encryptedString)
-	decryptedString, err = DecryptBase64StringToString(key, encryptedString)
-	if err != nil {
-		t.Fatalf("Could not decrypt the string: %s", err.Error())
-	}
-	t.Logf("Decrypted string = '%s'.", decryptedString)
-	assert.Equal(t, stringToEncrypt, decryptedString, "The origin and processed strings were not equal.")
+	EncTest(t, key, stringToEncrypt)
 
 	//Test the encryption flag, then on
 	t.Logf("Testing turning back on encryption.")
 	IsEncryptionOn = true
 	stringToEncrypt = "Encryption turned on test."
-	t.Logf("String to encrypt = '%s'.", stringToEncrypt)
-	encryptedString, err = EncryptStringToBase64EncodedString(key, stringToEncrypt)
+	EncTest(t, key, stringToEncrypt)
+
+	//Test a 1000 sets of keys and strings to encrypt/decrypt
+	for index := 0; index < 10000; index++ {
+		//randomize the key
+		randKey := RandString(keyLength)
+		randStringLength := rand.Intn(100)
+		stringToEncrypt := RandString(randStringLength)
+		EncTest(t, randKey, stringToEncrypt)
+	}
+	return
+}
+
+func EncTest(t *testing.T, keyToTest string, stringToTest string) {
+	t.Logf("Key used to encrypt = '%s'.", keyToTest)
+	t.Logf("String to encrypt = '%s'.", stringToTest)
+	encryptedString, err := EncryptStringToBase64EncodedString(keyToTest, stringToTest)
 	if err != nil {
 		t.Fatalf("Could not encrypt the string: %s", err.Error())
 	}
-	t.Logf("String as encrypted = '%s'.", encryptedString)
-	decryptedString, err = DecryptBase64StringToString(key, encryptedString)
+	decryptedString, err := DecryptBase64StringToString(keyToTest, encryptedString)
 	if err != nil {
 		t.Fatalf("Could not decrypt the string: %s", err.Error())
 	}
 	t.Logf("Decrypted string = '%s'.", decryptedString)
-	assert.Equal(t, stringToEncrypt, decryptedString, "The origin and processed strings were not equal.")
 
+	assert.Equal(t, stringToTest, decryptedString, "The origin and processed strings were not equal.")
+	return
 }
